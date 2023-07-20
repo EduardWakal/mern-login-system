25e5956bd212b5f185f4e91eb62fa01cd9356b4a
eduardwakal <eduard.wakal@nimbits.de>
2023-07-20 08:27:47 +0200

    add comments

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -3,32 +3,40 @@ import axios from "axios";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 
+// Die Auth-Komponente beinhaltet sowohl Login als auch Registrierung
 export const Auth = () => {
   return (
-    <div className="auth">
-      <Login />
-      <Register />
-    </div>
+      <div className="auth">
+        <Login />
+        <Register />
+      </div>
   );
 };
 
+// Login-Komponente
 const Login = () => {
+  // Nutzung von Cookies zum Speichern des Zugriffstokens
   const [_, setCookies] = useCookies(["access_token"]);
 
+  // Verwendung von useState für Benutzername und Passwort
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  // Verwendung von useNavigate für die Navigation nach der Anmeldung
   const navigate = useNavigate();
 
+  // Funktion zum Einreichen der Anmeldedaten
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
+      // Anfrage an den Server zum Einloggen
       const result = await axios.post("http://localhost:3001/auth/login", {
         username,
         password,
       });
 
+      // Speichern des Zugriffstokens im Cookie und Weiterleitung zur Startseite
       setCookies("access_token", result.data.token);
       window.localStorage.setItem("userID", result.data.userID);
       navigate("/");
@@ -37,44 +45,50 @@ const Login = () => {
     }
   };
 
+  // Formular für die Benutzeranmeldung
   return (
-    <div className="auth-container">
-      <form onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+      <div className="auth-container">
+        <form onSubmit={handleSubmit}>
+          <h2>Login</h2>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
   );
 };
 
+// Registrierungs-Komponente
 const Register = () => {
+  // Nutzung von useState für Benutzername und Passwort
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  // Nutzung von Cookies und useNavigate, wie bei der Login-Komponente
   const [_, setCookies] = useCookies(["access_token"]);
   const navigate = useNavigate();
 
+  // Funktion zum Einreichen der Registrierungsdaten
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      // Anfrage an den Server zur Registrierung
       await axios.post("http://localhost:3001/auth/register", {
         username,
         password,
@@ -85,30 +99,31 @@ const Register = () => {
     }
   };
 
+  // Formular für die Benutzerregistrierung
   return (
-    <div className="auth-container">
-      <form onSubmit={handleSubmit}>
-        <h2>Register</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
-    </div>
+      <div className="auth-container">
+        <form onSubmit={handleSubmit}>
+          <h2>Register</h2>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      </div>
   );
 };
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,54 +1,84 @@
+// Importieren der erforderlichen Module
 import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
+// Erstellen eines neuen Express-Routers
 const router = express.Router();
+
+// Importieren des User-Models
 import {UserModel} from "../models/Users.js";
 
+// Route für die Registrierung eines neuen Benutzers
 router.post("/register", async (req, res) => {
-    const {username, password} = req.body;
+    const {username, password} = req.body; // Auslesen von Benutzername und Passwort aus der Anfrage
+
+    // Prüfen, ob der Benutzername bereits in der Datenbank existiert
     const user = await UserModel.findOne({username});
     if (user) {
+        // Wenn der Benutzername bereits existiert, sende einen Fehlerstatus
         return res.status(400).json({message: "Username already exists"});
     }
+
+    // Wenn der Benutzername noch nicht existiert, erstelle ein neues Benutzerobjekt
+    // Hashen des Passworts vor dem Speichern in der Datenbank
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new UserModel({username, password: hashedPassword});
+
+    // Speichern des neuen Benutzers in der Datenbank
     await newUser.save();
-    res.json({message: "User registered successfully"});
+    res.json({message: "User registered successfully"}); // Rückmeldung, dass die Registrierung erfolgreich war
 });
 
+// Route für das Einloggen eines Benutzers
 router.post("/login", async (req, res) => {
-    const {username, password} = req.body;
+    const {username, password} = req.body; // Auslesen von Benutzername und Passwort aus der Anfrage
 
+    // Suche nach dem Benutzer in der Datenbank
     const user = await UserModel.findOne({username});
 
+    // Wenn der Benutzer nicht gefunden wurde oder das Passwort nicht stimmt, sende einen Fehlerstatus
     if (!user) {
         return res
             .status(400)
             .json({message: "Username or password is incorrect"});
     }
+
+    // Prüfung, ob das eingegebene Passwort mit dem in der Datenbank übereinstimmt
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
         return res
             .status(400)
             .json({message: "Username or password is incorrect"});
     }
+
+    // Wenn das Passwort korrekt ist, erstelle ein neues JWT (JSON Web Token)
     const token = jwt.sign({id: user._id}, "secret");
+
+    // Senden des Tokens und der Benutzer-ID als Antwort
     res.json({token, userID: user._id});
 });
 
+// Exportieren des Routers
 export {router as userRouter};
 
+// Funktion zur Überprüfung des JWTs
 export const verifyToken = (req, res, next) => {
+    // Auslesen des Authentifizierungs-Headers
     const authHeader = req.headers.authorization;
     if (authHeader) {
+        // Überprüfen des Tokens
         jwt.verify(authHeader, "secret", (err) => {
+            // Wenn der Token ungültig ist, sende einen Statuscode 403 (Zugriff verweigert)
             if (err) {
                 return res.sendStatus(403);
             }
+
+            // Wenn der Token gültig ist, führe die nächste Middleware-Funktion aus
             next();
         });
     } else {
+        // Wenn kein Token gesendet wurde, sende einen Statuscode 401 (nicht autorisiert)
         res.sendStatus(401);
     }
 };
